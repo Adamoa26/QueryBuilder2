9336746025a507ee4228a3b25884c74a55dd4667
Adam Elliott <adam.elliott@ou.edu>
2015-01-14 15:23:11 -0600

    All the local features seem to be working well. This is my first python package so we might have some trouble getting it installed, etc.

--- __main__.py
+++ __main__.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 import argparse
 from modules.queries import *
-from features.macfinder.sshcommands import *
+from querybuilder.features.macfinder.sshcommands import SSHConnection
 
 parser = argparse.ArgumentParser(prog="QueryBuilder",
-							 description="This program builds queries more quickly than the GUI in Kibana")
-parser.add_argument("-es", "--elastic", help="This will run the query ElasticSearch")
-parser.add_argument("-bro", "--brologs", help="This option will run the query on Bro Master")
-parser.add_argument("-conf", "--configure", action="store_true", help="This option will run the configuration setup.",)
+								description="This program builds queries more quickly than the GUI in Kibana")
+parser.add_argument("-es", "--elastic",
+					help="This will run the query ElasticSearch")
+parser.add_argument("-bro", "--brologs",
+					help="This option will run the query on Bro Master")
+parser.add_argument("-conf", "--configure",
+					action="store_true",
+					help="This option will run the configuration setup.",)
+
 
 args = parser.parse_args()
 
@@ -20,7 +25,7 @@ elif args.brologs:
 	bro = bromacfinder(filepath)
 	bro.run()
 elif args.configure:
-	features.macfinder.sshcommands.configuressh()
+	configure = SSHConnection()
+	configure.configuressh()
 else:
 	print "Not a valid entry. Need to specify [OPTION] [INPUT]"
-
